c431a1d790d97a008ff3acd4ac7bec6776cf0548
Simon Neutert <simon@Jerohimomo.fritz.box>
2020-10-11 11:52:55 +0200

    fix: apply prettier rules

--- jquery-google-reviews.js
+++ jquery-google-reviews.js
@@ -10,9 +10,9 @@ made me think and remix their work into the following lines.
 Thank you guys!
 */
 
-(function($) {
+(function ($) {
 
-  $.fn.googlePlaces = function(options) {
+  $.fn.googlePlaces = function (options) {
     // This is the easiest way to have default options.
     var settings = $.extend({
       // These are the defaults.
@@ -35,25 +35,25 @@ Thank you guys!
     var targetDiv = this[0];
     var targetDivJquery = this;
 
-    var renderMoreReviewsButton = function() {
-      return '<div class="more-reviews"><a href="'+settings.moreReviewsButtonUrl+'" target="_blank">'+settings.moreReviewsButtonLabel+'</a></div>';   
+    var renderMoreReviewsButton = function () {
+      return '<div class="more-reviews"><a href="' + settings.moreReviewsButtonUrl + '" target="_blank">' + settings.moreReviewsButtonLabel + '</a></div>';
     };
 
-    var renderWriteReviewButton = function() {
-      return '<div class="write-review"><a href="'+settings.writeReviewButtonUrl+'" target="_blank">'+settings.writeReviewButtonLabel+'</a></div>';   
+    var renderWriteReviewButton = function () {
+      return '<div class="write-review"><a href="' + settings.writeReviewButtonUrl + '" target="_blank">' + settings.writeReviewButtonLabel + '</a></div>';
     };
 
-    var renderPicture = function(picture) {
-      return "<img class='review-picture' src='"+picture+"'>";
+    var renderPicture = function (picture) {
+      return "<img class='review-picture' src='" + picture + "'>";
     }
 
-    var renderHeader = function(header) {
+    var renderHeader = function (header) {
       var html = "";
       html += header + "<br>";
       targetDivJquery.append(html);
     };
 
-    var renderFooter = function(footer) {
+    var renderFooter = function (footer) {
       var html = "";
       var htmlButtons = "";
 
@@ -64,14 +64,14 @@ Thank you guys!
         htmlButtons += renderWriteReviewButton();
       }
       if (htmlButtons != "") {
-        html += '<div class="buttons">'+htmlButtons+'</div>';
+        html += '<div class="buttons">' + htmlButtons + '</div>';
       }
-      
+
       html += "<br>" + footer + "<br>";
       targetDivJquery.after(html);
     };
 
-    var shortenName = function(name) {
+    var shortenName = function (name) {
       if (name.split(" ").length > 1) {
         var shortenedName = "";
         shortenedName = name.split(" ");
@@ -89,7 +89,7 @@ Thank you guys!
       }
     };
 
-    var renderStars = function(rating) {
+    var renderStars = function (rating) {
       var stars = '<div class="review-stars"><ul>';
       // fills gold stars
       for (var i = 0; i < rating; i++) {
@@ -105,14 +105,14 @@ Thank you guys!
       return stars;
     };
 
-    var convertTime = function(UNIX_timestamp) {
+    var convertTime = function (UNIX_timestamp) {
       var newDate = new Date(UNIX_timestamp * 1000);
       var months = settings.months;
       var time = newDate.getDate() + ". " + months[newDate.getMonth()] + " " + newDate.getFullYear();
       return time;
     };
 
-    var filterReviewsByMinRating = function(reviews) {
+    var filterReviewsByMinRating = function (reviews) {
       if (reviews === void 0) {
         return [];
       } else {
@@ -126,25 +126,25 @@ Thank you guys!
       }
     };
 
-    var sortReviewsByDateDesc = function(reviews) {
+    var sortReviewsByDateDesc = function (reviews) {
       if (typeof reviews != "undefined" && reviews != null && reviews.length != null && reviews.length > 0) {
-        return reviews.sort(function(a,b) {return (a.time > b.time) ? 1 : ((b.time > a.time) ? -1 : 0);} ).reverse();
+        return reviews.sort(function (a, b) { return (a.time > b.time) ? 1 : ((b.time > a.time) ? -1 : 0); }).reverse();
       } else {
         return []
       }
     }
 
-    var sanitizedReviewText = function(text) {
+    var sanitizedReviewText = function (text) {
       text = text.replace("<script>", "");
       text = text.replace("<iframe>", "");
       return text
     }
 
-    var renderReviews = function(reviews) {
+    var renderReviews = function (reviews) {
       reviews.reverse();
       var html = "";
       var rowCount = (settings.maxRows > 0) ? settings.maxRows - 1 : reviews.length - 1;
-      
+
       rowCount = (rowCount > reviews.length - 1) ? reviews.length - 1 : rowCount;
       for (var i = rowCount; i >= 0; i--) {
         var picture = "";
@@ -154,16 +154,16 @@ Thank you guys!
         var date = convertTime(review.time);
         var name = settings.shortenNames ? shortenName(review.author_name) : review.author_name;
         var style = (reviewText.length > parseInt(settings.textBreakLength)) ? "review-item-long" : "review-item";
-        
-        if(settings.showProfilePicture) {
-          picture = renderPicture(review.profile_photo_url); 
+
+        if (settings.showProfilePicture) {
+          picture = renderPicture(review.profile_photo_url);
         }
 
         if (settings.showReviewDate == true) {
           review_text = "<span class='review-date'>" + date + "</span> " + review_text;
         }
 
-        html = html + "<div class="+ style +"><div class='review-header'>"+ picture +"<div class='review-usergrade'><div class='review-meta'><span class='review-author'>" + name + "</span><span class='review-sep'></span>" + "</div>" + stars + "</div></div><p class='review-text'>" + reviewText + "</p></div>";
+        html = html + "<div class=" + style + "><div class='review-header'>" + picture + "<div class='review-usergrade'><div class='review-meta'><span class='review-author'>" + name + "</span><span class='review-sep'></span>" + "</div>" + stars + "</div></div><p class='review-text'>" + reviewText + "</p></div>";
         // I do not need to display the date... but if you do:
         // +"<br><span class='review-date'>"+date+"</span>"+
       }
@@ -181,7 +181,7 @@ Thank you guys!
     };
 
     // the callback is what initiates the rendering if Status returns OK
-    var callback = function(place, status) {
+    var callback = function (place, status) {
       if (status == google.maps.places.PlacesServiceStatus.OK) {
         var filteredReviews = filterReviewsByMinRating(place.reviews);
         var sortedReviews = sortReviewsByDateDesc(filteredReviews);
@@ -193,7 +193,7 @@ Thank you guys!
       }
     }
 
-    return this.each(function() {
+    return this.each(function () {
       // Runs the Plugin
       if (settings.placeId === undefined || settings.placeId === "") {
         console.error("NO PLACE ID DEFINED");
